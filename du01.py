e576abcf2bb5654fa12ccf6e0e5117162f713973
kholjir <kholjir@gmail.com>
2022-11-12 22:53:24 +0100

    modified:   du01.py 	new file:   texty/slova.txt 	modified:   texty/wiki.txt

--- du01.py
+++ du01.py
@@ -1,3 +1,18 @@
-with open("texty/wiki.txt", "r", encoding="utf-8") as soubor:
-    obsah_souboru = soubor.readlines()
-    print(obsah_souboru)
+def readB(x):
+ with open('C:/Git/PRO2223/PRO2223/texty/wiki.txt', encoding="utf8") as soubor:
+    for int, line in enumerate(soubor):
+        if x in line:
+            print('Text obsahuje zakázané slovo', x)
+            print('Čislo řádku:', int)
+            print('Řadek:', line)
+          
+             
+def readA():
+ with open('C:/Git/PRO2223/PRO2223/texty/slova.txt', encoding="utf8") as slova:
+    for line in slova:
+        readB(line)
+
+readA()
+
+
+  
