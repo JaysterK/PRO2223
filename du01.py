fd27f9979a592de3e4b46e1f14b76e1308313a46
kholjir <kholjir@gmail.com>
2022-11-13 18:05:27 +0100

    modified:   du01.py

--- du01.py
+++ du01.py
@@ -5,14 +5,10 @@ def readB(x):
             print('Text obsahuje zakázané slovo', x)
             print('Čislo řádku:', int)
             print('Řadek:', line)
-          
-             
+              
 def readA():
  with open('C:/Git/PRO2223/PRO2223/texty/slova.txt', encoding="utf8") as slova:
     for line in slova:
         readB(line)
 
-readA()
-
-
-  
+readA() 
