0ddda6d602bd49e6a1c520661cab306cba5a564b
kholjir <kholjir@gmail.com>
2022-11-16 12:02:57 +0100

    lol

---
+++ cv09.py
@@ -0,0 +1,12 @@
+import matplotlib
+import matplotlib.plot as plt
+import numpy
+#www.pypi.org
+
+x = [0,1,2,3,4,5,6,7,8,9]
+y = [5,5,4,5,3,5,2,2,2,1]
+plt.plot(x, y)
+plt.xlabel("Pořadí známek")
+plt.ylabel("Známka")
+plt.show
+
